671c377ea8562b7a1cde3d773c12d3274d12fc7f
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-04 20:54:06 +0100

    Add the form for the education

--- 08-MultiStepForm/multi-step-form/src/components/Steps/Education.jsx
+++ 08-MultiStepForm/multi-step-form/src/components/Steps/Education.jsx
@@ -1,7 +1,37 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useAppState } from '../../contexts/state';
+import { Button, Field, Form, Input } from '../Forms';
+
 const Education = () => {
+  const [state, setState] = useAppState();
+  const { handleSubmit, register } = useForm({ defaultValues: state });
+  const navigate = useNavigate();
+
+  const saveData = (data) => {
+    setState({ ...state, ...data });
+    navigate('/about');
+  };
+
   return ( 
-    <h1>Education</h1>
+    <Form onSubmit={handleSubmit(saveData)}>
+      <fieldset>
+        <legend>Education</legend>
+        <Field label="University">
+          <Input {...register('university')} id='university' />
+        </Field>
+        <Field label='Degree'>
+          <Input {...register('degree')} id='degree' />
+        </Field>
+        <div className='button-row'>
+          <Button variant='secondary' onClick={() => navigate('/')}>
+            {'<'} Previous
+          </Button>
+          <Button>Next {'>'}</Button>
+        </div>
+      </fieldset>
+    </Form>
   );
-}
+};
  
 export default Education;
