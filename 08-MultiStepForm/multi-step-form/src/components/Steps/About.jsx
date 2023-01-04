de71db7a38ca3de6b4061c8bfa147f4276700209
Alexandre-st <alexandre.saintprix@gmail.com>
2023-01-04 21:37:52 +0100

    Add about form

--- 08-MultiStepForm/multi-step-form/src/components/Steps/About.jsx
+++ 08-MultiStepForm/multi-step-form/src/components/Steps/About.jsx
@@ -1,6 +1,33 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useAppState } from "../../contexts/state";
+import { Button, Field, Form } from "../Forms";
 const About = () => {
+  
+  const [state, setState] = useAppState();
+  const { handleSubmit, register } = useForm({ defaultValues: state });
+  const navigate = useNavigate();
+
+  const saveData = (data) => {
+    setState({ ...state, ...data });
+    navigate('/confirm');
+  };
+
   return (
-    <h1>About</h1>
+    <Form onSubmit={handleSubmit(saveData)}>
+      <fieldset>
+        <legend>About</legend>
+        <Field label='About Me'>
+          <textarea {...register('about')} id='about' className='form-control' />
+        </Field>
+        <div className="button-row">
+          <Button variant='secondary' onClick={() => navigate('/education')}>
+            {'<'} Previous
+          </Button>
+          <Button>Next {'>'}</Button>
+        </div>
+      </fieldset>
+    </Form>
   );
 };
  
