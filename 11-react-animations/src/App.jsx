bc6534036c96185fc9aeb847aaa3ce7c0732e962
Alexandre-st <alexandre.saintprix@gmail.com>
2023-11-22 22:12:34 +0100

    Add a code but not working

--- 11-react-animations/src/App.jsx
+++ 11-react-animations/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Autoplay, EffectFade } from 'swiper';
+import "swiper/css/effect-fade";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/scss";
+import { donnees } from "./data";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>Hello World  !</h1>
+    <Swiper 
+      modules={[Autoplay, EffectFade]}
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      effect="fade"
+      fadeEffect={{
+        crossFade: true
+      }}
+      className="mySwiper"
+    >
+      {donnees.map((item, index) => (
+        <SwiperSlide key={index} className="">
+          <img src={item.img} alt={item.name} />
+          <p>{item.name}</p>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ 11-react-animations/src/data.js
@@ -0,0 +1,18 @@
+import advice from "./assets/advice.jpg";
+import github from "./assets/github_user.png";
+import todo from "./assets/todoList.png";
+
+export const donnees = [
+  {
+    "img": advice,
+    "name": "Advice App",
+  },
+  {
+    "img": github,
+    "name": "Github User App",
+  },
+  {
+    "img": todo,
+    "name": "Todo List App",
+  }
+]
